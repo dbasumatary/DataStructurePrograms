3cddfd41546a2d9cf59c73e7f8539bdc4cdb0d09
dbasumatary <basudhondip@gmail.com>
2023-03-08 08:55:18 +0530

    Added: Palindrome check

---
+++ src/com/palindrome/Palindrome.java
@@ -0,0 +1,32 @@
+package com.palindrome;
+import java.util.Scanner;
+
+//Check for palindrome
+public class Palindrome {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+}
